7633054c5b8962ebdde5db3e15712ed0a1ed057b
MasterJhonny <jhonnyeddy.22@gmail.com>
2021-06-02 13:24:26 -0400

    se inincia las peticiones para el reto

--- src/callback/index.js
+++ src/callback/index.js
@@ -22,12 +22,49 @@
 // }
 
 // data(printDate)
-var valor = 1
-console.log('primera llamada')
-function hacerllamada(valor) {
-    console.log(`segunda llamada ${valor}`)
+// var valor = 1
+// console.log('primera llamada')
+// function hacerllamada(valor) {
+//     console.log(`segunda llamada ${valor}`)
+// }
+
+// for(let i = 0; i < 4; i++) {
+//     setTimeout( () => hacerllamada(i), 3000);
+// }
+
+// console.log('taco 1')
+// console.log('taco 2')
+// setTimeout( () => console.log('Torta'), 0);
+// console.log('taco 3')
+
+// practice..
+function suma(num1, num2){
+    return num1 + num2
 }
 
-for(let i = 0; i < 4; i++) {
-    setTimeout( () => hacerllamada(i), 3000);
+function resta(num1, num2){
+    return num1 - num2
 }
+
+function calcular(callback){
+    return callback()
+}
+console.log(resta(4, 4))
+calcular(() => {
+    setTimeout(() => console.log(suma(2, 5)), 0)
+})
+console.log(4)
+
+function data(callback){
+    console.log(new Date)
+    setTimeout(() => {
+        let dato = new Date;
+        callback(dato);
+    }, 3000)
+}
+
+function printDato(valor){
+    console.log(valor)
+}
+
+data(printDato)
